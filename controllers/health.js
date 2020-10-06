f9ab73b2c7f93dd8d8ff0a0b212140757bdfa2e6
Pablo Albarrazin <pablo.albarrazin@pedidosya.com>
2020-10-06 20:14:20 -0300

    Add some console logs

--- controllers/health.js
+++ controllers/health.js
@@ -5,5 +5,13 @@ router
 .get('/health', (ctx, next) => {
     ctx.body = {status: 'ok'}
 })
+.get('/error', (ctx, next) => {
+    try {
+        throw new Error("My custom error message")
+    } catch (e) {
+        console.log(e)
+    }
+    ctx.body = {status: 'error'}
+})
 
 module.exports = router
--- controllers/posts.js
+++ controllers/posts.js
@@ -3,18 +3,22 @@ const router = new Router()
 
 router
 .get('/posts', async (ctx, next) => {
+    console.log("Trying to get all blog posts")
     ctx.body = await ctx.db.models.Post.findAll()
 })
 .get('/posts/:id', async (ctx, next) => {
+    console.log("Trying to get post with id " + ctx.params.id)
     ctx.body = await ctx.db.models.Post.findAll({ where: { id: ctx.params.id } })
 })
 .post('/posts', async (ctx, next) => {
     let body = ctx.request.body
+    console.log("Trying to create post with body " + JSON.stringify())
     ctx.body = await ctx.db.models.Post.create({ title: body.title, content: body.content })
 })
 .put('/posts/:id', async (ctx, next) => {
     let post = await ctx.db.models.Post.findByPk(ctx.params.id)
     if(post !== null) {
+        console.log("Trying to update post with body " + ctx.params.id)
         let body = ctx.request.body
         post.update({
             title: body.title,
@@ -27,6 +31,7 @@ router
 })
 .del('/posts/:id', async (ctx, next) => {
     let post = await ctx.db.models.Post.findByPk(ctx.params.id)
+    console.log("Trying to delete post with id " + ctx.params.id)
     if(post !== null) {
         post.destroy()
     }
